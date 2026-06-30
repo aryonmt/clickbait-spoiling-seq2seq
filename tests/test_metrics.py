248a814cb9b776c5c28008ff4c59524530c54836
aryo <aramne12@gmail.com>
2026-06-30 17:21:45 +0330

    test: fix BLEU-4 short sequence edge case in metrics tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,15 +2,17 @@ from spoiler_gen.evaluation.metrics import compute_bleu4, compute_metrics_by_spo
 
 
 def test_compute_bleu4():
-    preds = ["this is a test", "hello world"]
-    refs = ["this is a test", "hello world"]
+    preds = ["this is a test", "hello world nice day"]
+    refs = ["this is a test", "hello world nice day"]
     score = compute_bleu4(preds, refs)
     assert score > 99.0
 
 
 def test_compute_metrics_by_spoiler_type():
-    preds = ["yes", "no", "maybe"]
-    refs = ["yes", "no", "maybe"]
+    # Upgrade test sentences to contain at least 4 words each
+    # This ensures BLEU-4 is calculable (not 0.0 due to short length) when grouped by type
+    preds = ["this is a phrase", "this is a passage", "this is a multi"]
+    refs = ["this is a phrase", "this is a passage", "this is a multi"]
     spoiler_types = ["phrase", "passage", "multi"]
     results = compute_metrics_by_spoiler_type(preds, refs, spoiler_types)
     assert "all" in results
